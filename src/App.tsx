bdc4c13aaf3f175433030b2ae69ae226321c0287
Alvin Leung <alvinleung2009@gmail.com>
2022-06-02 12:53:28 -0700

    basic functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Project from "./components/Project";
+
+const RepeatableGrid = () => (
+  <>
+    <Project name="Inspired" />
+    <div />
+    <div />
+    <div />
+    <div />
+    <Project name="Prologue" />
+    <div />
+    <Project name="Work Week" />
+    <div />
+    <Project name="Almost Fun" />
+    <div />
+    <div />
+    <div />
+    <div />
+    <Project name="Inspired" />
+    <Project name="Party Round" />
+    <div />
+    <div />
+    <div />
+    <div />
+    <Project name="Inspired" />
+  </>
+);
 
 function App() {
   return (
-    <div>
-      <p>Clean React App</p>
+    <div className="w-full h-screen grid grid-cols-3 gap-4 p-4 overflow-x-hidden">
+      <RepeatableGrid />
+      <RepeatableGrid />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
